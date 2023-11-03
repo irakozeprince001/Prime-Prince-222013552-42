c628aa133884dbd172f7b309e16c88a19ca5d4aa
irakozeprince001 <irakozeprince2@gmail.com>
2023-11-03 11:32:14 -0700

    switch

---
+++ switchcase3.java
@@ -0,0 +1,19 @@
+class switchcase3 {
+    public static void main(String[] args) {
+        int number = 2;
+        switch (number) {
+            case 1:
+                System.out.println("Case 1: The number is 1");
+                break;
+            case 2:
+                System.out.println("Case 2: The number is 2");
+                break;
+            case 3:
+                System.out.println("Case 3: The number is 3");
+                break;
+            default:
+                System.out.println("The number is not 1, 2, or 3");
+                break;
+        }
+    }
+}
